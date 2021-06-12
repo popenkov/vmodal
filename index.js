85a7d37b7b8a41bee59fc53b373b9cd2d6d11a18
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-06-12 12:02:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const fruits = [
+    {id: 1, title: "Яблоки", price: 20, img = 'https://lifeglobe.net/x/entry/6259/1a-0.jpg'},
+    {id: 2, title: "Апельсины", price: 30, img = 'https://lh3.googleusercontent.com/proxy/1KlJmztMgygXX7uulFWGcH58FcybPdZWVOQbh-BerRLQVnFzUbEbq_f5G2TGahWx3jr6MvHXwUz4dDxYurmwR6MtiIKvysQgPTFY1qhnpeHMeU9joUZz_RQBHuHfsuE'},
+    {id: 3, title: "Манго", price: 40, img = 'https://zamorozka54.ru/images/product/38_f18cf7bb1822.jpg'},
+]
+
+/* 
+1. динамически на основе массива вывести список карточек
+2. при клике на показать цену - показать модалку с ценой и одной кнопкой ок
+3. при клике на удалить - показать модалкуЖ удалить апельскины? ок отмена.
+----------------
+*4. удалить карточку из дом дерева при клике на ок. на основе $.modal сделать другой плагин $.confirm (Promise) 
+*/
+
+const myModal = $.modal({title: 'Пост 1', closable: true, content: `<h3>${'Пост номер 1'}</h3><p>${'Я делаю домашнее задание'}</p>`, width: '50%',
+footerButtons: [{
+    text: 'Ok',
+    type: 'primary', 
+    handler () {
+        console.log('primary btn clicked');
+        myModal.close();
+    }
+},
+
+    {text: 'Cancel',
+    type: 'danger', 
+    handler () {
+        console.log('danger btn clicked');
+        myModal.close();
+    }
+}]
+});
---
+++ plugins/modal.js
@@ -0,0 +1,121 @@
+Element.prototype.appendAfter = function (element) {
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop () {
+
+}
+
+function _createModalFooter (buttons = []) {
+    if (buttons.length === 0) {
+        return document.createElement('div');
+    }
+    const wrap = document.createElement('div');
+    wrap.classList.add('modal-footer');
+
+    buttons.forEach(btn => {
+        const $btn = document.createElement('button');
+        $btn.textContent = btn.text;
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick= btn.handler || noop;
+        wrap.appendChild($btn);
+    })
+    return wrap;
+}
+
+function _createModal(options) {
+    let {title, closable, content, width, footerButtons} = options;
+    const DEFAULT_WIDTH = '600px';
+    const modal = document.createElement('div');
+    modal.classList.add('vmodal');
+    modal.insertAdjacentHTML('afterBegin', `
+        <div class="modal-overlay" data-close="true">
+
+            <div class="modal-window" style="width: ${width || DEFAULT_WIDTH}">
+                <div class="modal-header">
+                    <span class="modal-title">${title || "Окно"}</span>                    
+                    ${closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
+                </div>
+                <div class="modal-body" data-content>
+                    ${content || ''}
+                </div>
+
+            </div>
+
+        </div>`
+    )
+
+    const footer = _createModalFooter(footerButtons);
+    footer.appendAfter(modal.querySelector('[data-content]'))
+    document.body.appendChild(modal);
+    return modal;
+}
+
+
+
+/* 
++++ title: string
++++ closable: boolean -показывать крестик или нет
++++ content: string html
++++ width: string ('400px')
+destroy(): void - удалять все слушатели и модальное окно
+---------------------------------------------------------
+на крестик и на клик вне контента окно должно закрываться
+
+********** публичный метод setContent(html: string): void
+lifecycle hookes: onClose(): void
+ onOpen(): void
+  beforeClose(): boolean
+  _____________
+  animate css lybrary
+*/
+
+$.modal = function (options) {
+    const ANIMATION_SPEED = 200;
+
+    const $modal = _createModal(options);
+
+    let closing = false;
+    let destroyed = false;
+
+    const modal = {
+        open () {
+            if (destroyed) {
+                console.log('Modal is destroyed')
+            }
+            
+            !closing && $modal.classList.add('open');  
+        },
+        close () {
+            closing=true;
+            $modal.classList.remove('open'); 
+            $modal.classList.add('hide');
+            setTimeout(() => {
+                $modal.classList.remove('hide');
+                closing = false;
+            }, ANIMATION_SPEED)  
+        }
+    }
+
+    const listener = evt => {
+        if (evt.target.dataset.close) {
+            modal.close();
+        }
+    };
+
+    $modal.addEventListener('click', listener);
+
+/*     const closeBtn = $modal.querySelector('.modal-close');
+    closeBtn.addEventListener('click', $modal.close()) */
+
+    return Object.assign(modal, {
+        destroy () {
+            $modal.parentNode.removeChild($modal);
+            $modal.removeEventListener('click', listener);
+            destroyed= true;
+        },
+        setContent (html) {
+            $modal.querySelector('[data-content]').innerHTML = html;
+        }
+    });
+}
